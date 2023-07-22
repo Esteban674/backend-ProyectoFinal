28617609d2db44826131e6d757d5446c8c48133e
Esteban Perea <estebanperea@gmail.com>
2023-07-22 11:43:03 -0300

    Correccion en router products

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -7,7 +7,7 @@ const routerProduct = Router();
 routerProduct.get('/', productController.getAllProducts);
 routerProduct.get('/:pid', productController.getProductById);
 routerProduct.post('/', passportCall('jwt'), authorization('Admin'), productController.addProduct);
-routerProduct.delete('/:pid', authorization('Admin'), productController.deleteProduct);
+routerProduct.delete('/:pid', productController.deleteProduct);
 routerProduct.put('/:pid', productController.updateProduct);
 
 export default routerProduct;
